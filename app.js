a1133c9ab076daa6470b63589b9c2c6266697a89
Josseth Sanchez <128737955+Aine20@users.noreply.github.com>
2023-06-29 02:49:54 +0000

    Testing with Jest

---
+++ app.js
@@ -0,0 +1,24 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+
+let fromEuroToDollar = function(euroValue){
+	return euroValue * 1.2;
+}
+
+let fromDollarToYen = function(dollarValue){
+	return dollarValue * 127.9;
+}
+
+let fromYenToPound = function(yenValue){
+	return yenValue * 0.8;
+}
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
